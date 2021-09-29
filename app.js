f4206d7a0709c069d777bf76e1a585935be75f01
James Demiraiakian <james.armen@gmail.com>
2021-09-29 11:04:31 -0700

    JavaScript practice notes

--- app.js
+++ app.js
@@ -1,8 +1,46 @@
-// import functions and grab DOM elements
+//string
+console.log('hello world');
 
-// initialize global state
+//numbers
+console.log(1 + 1);
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+//empty
+console.log(undefined);
+console.log(null);
+
+//variables
+
+const PI = 3.1415926;
+
+console.log(PI);
+console.log(`The area of a circle with radius 2 is: ${PI * 2 * 2}`);
+
+console.log('4' + '2');
+console.log(4 + '2');
+console.log(4 + Number('2'));
+console.log(4 + 2);
+
+//arrays
+let myListNum = [1, 2, 3, 4];
+let names = ['James', 'Caroline', 'Ellie', 'Alexis'];
+let mixed = [1, 'Caroline', 2, 'James'];
+
+console.log(myListNum);
+console.log(names);
+console.log(mixed);
+
+let colors = ['purple', 'forest green', 'black'];
+console.log(colors);
+console.log(colors[0]);
+
+colors[2] = 'grey';
+console.log(colors);
+
+colors.push('yellow');
+console.log(colors);
+
+colors.pop();
+console.log(colors);
+
+colors.shift();
+console.log(colors);
